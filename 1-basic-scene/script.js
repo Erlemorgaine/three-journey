5f9cc438669aef285d394f2fb7a8445d29ba9286
Erle Monfils <erlemonfils@TVA053.wind3.hub>
2022-02-25 20:38:58 +0100

    lesson 1

--- 1-basic-scene/script.js
+++ 1-basic-scene/script.js
@@ -1,1 +1,40 @@
-console.log("HI THERE");
+const scene = new THREE.Scene();
+
+const geom = new THREE.BoxGeometry(1, 1, 1);
+const mat = new THREE.MeshBasicMaterial({ color: "green" });
+const cube = new THREE.Mesh(geom, mat);
+
+scene.add(cube);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+/**
+ * Params:
+ *
+ * fov: vertical size of field of camera.
+ * - with a large fov you can see far on left and right
+ * - with a small one, you can see a small field very far away from you.
+ *  and far objects will look more distorted
+ *
+ * aspectRatio: width / height of canvas
+ *
+ * renderer: Renders scene, but seen through camera pov
+ *
+ *
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.getElementById("webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
